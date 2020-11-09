0f14f5f20d2ecf5c8caea0b548c9fdf8b781e096
Cyril Leclerc <cyril@dashlane.com>
2020-11-09 23:31:54 +0100

    cleanup

--- pouf.py
+++ pouf.py
@@ -31,11 +31,8 @@ def title_text(draw, y, width, text):
     draw.text((x, y), text=text, fill="yellow")
 
 def main():
-    # regulator = framerate_regulator(fps=10)
-    # banana = Image.open(img_path)
     size = [min(*device.size)] * 2
-    posn = ((device.width - size[0]) // 2, device.height - size[1])
-    host = "192.168.33.30"
+    host = "192.168.33.30" # set host if it picks the wrong one.
     token = open('mytokenfile').read()
     old_image_url = ''
     roonapid = roonapi.RoonApi(appinfo, token, host=host)
@@ -46,84 +43,74 @@ def main():
     last_zone = ''
     background = Image.new("RGB", device.size, "black")
     image = None
+    margin = 4 # :p
+    current = 0
+    length = 0
+    playing = ''
+    playing2 = ''
+    zone = False
+
     while True:
-        idx = 0
-        found = 0
+
         found_zone = False
         for zone in roonapid.zones:
             if 'state' in roonapid.zones[zone]:
                 state = roonapid.zones[zone]['state']
             if state == "playing" and 'seek_position' in roonapid.zones[zone]:
-                found = idx
                 found_zone = zone
                 last_zone = zone
-            idx = idx + 1
 
-        
-
-        if not found:
+        if not found_zone:
           time.sleep(1)
           # device.display(background)
-
-        margin = 4 # :p
-        playing = ''
-        playing2 = ''
-        current = 0
-        length = 0
 
         if not found_zone:
            if last_zone:
               found_zone=last_zone
            else:
               found_Zone=next(iter(roonapid.zones.values()))
+              
+        if found_zone:
+          zone_id = found_zone['zone_id'] if isinstance(found_zone,dict) else found_zone
+          zone = roonapid.zones[zone_id]
 
-        if isinstance(found_zone,dict):
-           zone = roonapid.zones[found_zone['zone_id']]
-        else:
-           if found_zone:
-              zone = roonapid.zones[found_zone]
-        
-        
         if 'state' in zone:
             state = zone['state']
             
             if "now_playing" in zone:
-                now=zone['now_playing']
-                playing=now['two_line']['line1']
-                playing2=now['two_line']['line2']
+                now = zone['now_playing']
+                playing = now['two_line']['line1']
+                playing2 = now['two_line']['line2']
                 
                 if "image_key" in now: 
-                  image_url=roonapid.get_image(now['image_key']).replace('=500','=64',1)
-                  # image_url=image_url_temp.replace('=500','=64',1)
+                  image_url = roonapid.get_image(now['image_key']).replace('=500','=64',1)
                   if image_url != old_image_url:
-                      image=Image.open(requests.get(image_url, stream=True).raw)
-                      old_image_url=image_url
+                      image = Image.open(requests.get(image_url, stream=True).raw)
+                      old_image_url = image_url
                   background = Image.new("RGB", device.size, "black")
                   background.paste(image.resize(size, resample=Image.LANCZOS), (192,0))
 
                 if "length" in now:
-                  length=now['length']
+                  length = now['length']
                 if state == "playing" and 'seek_position' in zone:
-                  current=zone['seek_position']
+                  current = zone['seek_position']
                 elif "seek_position" in now:
-                  current=now['seek_position']
+                  current = now['seek_position']
             else:
               image = Image.new("RGB", device.size, "black")
-
-        
             
         draw = ImageDraw.Draw(background)
         if 'state' in zone:
           title_text(draw, margin, 256, "%s - %s" % (zone['display_name'], state) )
+
         if current:
           draw.text( (margin, 50), text="%s / %s" % (str(datetime.timedelta(seconds=current)), str(datetime.timedelta(seconds=length)) ) )
+          
         draw.text( (margin, 20), text=playing, font=tiny_font)
         draw.text( (margin, 35), text=playing2, font=tiny_font)
 
         device.display(background)
         time.sleep(1)
-
-
 
 if __name__ == "__main__":
     try:
