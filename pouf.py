caffac0ff86fc655ff9c64ef19eb567fffd30def
Cyril Leclerc <cyril@dashlane.com>
2020-11-10 00:02:37 +0100

    autopep

--- pouf.py
+++ pouf.py
@@ -19,36 +19,38 @@ import datetime
 from hotspot.common import title_text, right_text, tiny_font
 
 appinfo = {
-        "extension_id": "roon_oled_info",
-        "display_name": "Python library oled info for Roon",
-        "display_version": "1.0.0",
-        "publisher": "tidalf",
-        "email": "tidalf@ematome.com"
-    }
-  
+    "extension_id": "roon_oled_info",
+    "display_name": "Python library oled info for Roon",
+    "display_version": "1.0.0",
+    "publisher": "tidalf",
+    "email": "tidalf@ematome.com"
+}
+
+
 def title_text(draw, y, width, text):
     x = (width - draw.textsize(text)[0]) / 2
     draw.text((x, y), text=text, fill="yellow")
 
+
 def main():
-    size = [min(*device.size)] * 2
-    host = "192.168.33.30" # set host if it picks the wrong one.
-    token = open('mytokenfile').read()
-    old_image_url = ''
-    roonapid = roonapi.RoonApi(appinfo, token, host=host)
-    # save the token for next time
-    with open('mytokenfile', 'w') as f:
-      f.write(roonapid.token)# save the token for next time
-    # image_composition = ImageComposition(device)
-    last_zone = ''
-    background = Image.new("RGB", device.size, "black")
     image = None
-    margin = 4 # :p
+
+    margin = 4  # :p
     current = 0
     length = 0
     playing = ''
     playing2 = ''
-    zone = False
+    old_image_url = ''
+    size = [min(*device.size)] * 2
+    token_file = '.roon_token'
+    host = "192.168.33.30"  # set host if it picks the wrong one.
+    token = open(token_file).read()
+    roonapid = roonapi.RoonApi(appinfo, token, host=host)
+    # save the token for next time
+    with open(token_file, 'w') as f:
+        f.write(roonapid.token)  # save the token for next time
+    last_zone = ''
+    background = Image.new("RGB", device.size, "black")
 
     while True:
 
@@ -61,56 +63,59 @@ def main():
                 last_zone = zone
 
         if not found_zone:
-          time.sleep(1)
-          # device.display(background)
+            if last_zone:
+                found_zone = last_zone
+            else:
+                found_Zone = next(iter(roonapid.zones.values()))
 
-        if not found_zone:
-           if last_zone:
-              found_zone=last_zone
-           else:
-              found_Zone=next(iter(roonapid.zones.values()))
-              
         if found_zone:
-          zone_id = found_zone['zone_id'] if isinstance(found_zone,dict) else found_zone
-          zone = roonapid.zones[zone_id]
+            zone_id = found_zone['zone_id'] if isinstance(
+                found_zone, dict) else found_zone
+            zone = roonapid.zones[zone_id]
 
         if 'state' in zone:
             state = zone['state']
-            
+
             if "now_playing" in zone:
                 now = zone['now_playing']
                 playing = now['two_line']['line1']
                 playing2 = now['two_line']['line2']
-                
-                if "image_key" in now: 
-                  image_url = roonapid.get_image(now['image_key']).replace('=500','=64',1)
-                  if image_url != old_image_url:
-                      image = Image.open(requests.get(image_url, stream=True).raw)
-                      old_image_url = image_url
-                  background = Image.new("RGB", device.size, "black")
-                  background.paste(image.resize(size, resample=Image.LANCZOS), (192,0))
+
+                if "image_key" in now:
+                    image_url = roonapid.get_image(now['image_key']).replace(
+                        '=500', '=%s' % device.height, 1)
+                    if image_url != old_image_url:
+                        image = Image.open(requests.get(
+                            image_url, stream=True).raw)
+                        old_image_url = image_url
+                    background = Image.new("RGB", device.size, "black")
+                    background.paste(image.resize(
+                        size, resample=Image.LANCZOS), (device.width - device.height, 0))
 
                 if "length" in now:
-                  length = now['length']
+                    length = now['length']
                 if state == "playing" and 'seek_position' in zone:
-                  current = zone['seek_position']
+                    current = zone['seek_position']
                 elif "seek_position" in now:
-                  current = now['seek_position']
+                    current = now['seek_position']
             else:
-              image = Image.new("RGB", device.size, "black")
-            
+                image = Image.new("RGB", device.size, "black")
+
         draw = ImageDraw.Draw(background)
         if 'state' in zone:
-          title_text(draw, margin, 256, "%s - %s" % (zone['display_name'], state) )
+            title_text(draw, margin, device.width, "%s - %s" %
+                       (zone['display_name'], state))
 
         if current:
-          draw.text( (margin, 50), text="%s / %s" % (str(datetime.timedelta(seconds=current)), str(datetime.timedelta(seconds=length)) ) )
-          
-        draw.text( (margin, 20), text=playing, font=tiny_font)
-        draw.text( (margin, 35), text=playing2, font=tiny_font)
+            draw.text((margin, 50), text="%s / %s" % (str(datetime.timedelta(
+                seconds=current)), str(datetime.timedelta(seconds=length))))
+
+        draw.text((margin, 20), text=playing, font=tiny_font)
+        draw.text((margin, 35), text=playing2, font=tiny_font)
 
         device.display(background)
         time.sleep(1)
+
 
 if __name__ == "__main__":
     try:
